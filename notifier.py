bfc2e08e3cfe1d75073a90d806bf3dcaafdb7f4c
Muhammad Aliff Mohamed Anuar <75883097+17203058@users.noreply.github.com>
2023-05-05 12:30:16 +0800

    added keyboard response

--- notifier.py
+++ notifier.py
@@ -6,7 +6,12 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup,ReplyKey
 from telegram.ext import CallbackContext, Updater, PicklePersistence, CommandHandler, CallbackQueryHandler, \
     MessageHandler, ConversationHandler
 from dotenv import load_dotenv
-import os
+import uuid
+import io
+from PIL import Image
+import shutil
+
+
 
 
 
@@ -28,17 +33,77 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Reporting to duty sir.All system in check and fully operational')
 
 def alert(image):
-    keyboard = [['Trigger Alarm'],['Destroy Target'],['Notified Police'],['Add Target to Guest List']]
-    reply_markup = ReplyKeyboardMarkup(keyboard)
+    # filename=save_image(image)
+    # print(filename)
+    keyboard = [
+    [
+        InlineKeyboardButton("Trigger Alarm", callback_data="1"),
+        InlineKeyboardButton("Notified Police", callback_data="2"),
+        # InlineKeyboardButton("Add to Guest",callback_data=f"3 #{filename}"),
+
+    ],
+    [InlineKeyboardButton("Destroy Target", callback_data="4")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
     bot.send_photo(chat_id=chat_id, photo=image,caption="<b>Alert :</b> \nUnidentified person detected", reply_markup=reply_markup,parse_mode="HTML")
 
+
+def save_image(image):
+    # Set the folder path
+    folder_path = './detected_list'
+
+    # Generate a unique random ID
+    unique_id = str(uuid.uuid4())
+
+    # Check if a file with the same name already exists in the folder
+    filename = unique_id + '.jpg'
+    file_path = os.path.join(folder_path, filename)
+    while os.path.exists(file_path):
+        unique_id = str(uuid.uuid4())
+        filename = unique_id + '.jpg'
+        file_path = os.path.join(folder_path, filename)
+
+    # Create the image from the BytesIO object
+    image = Image.open(image)
+
+    # Save the image to the file path
+    image.save(file_path)
+    print(filename)
+    return filename
+
+def trigger_alarm(update,context):
+    print('alarm triggered')
+
+def notify_police(update,context):
+    print('police notified')
+
+def add_to_guest(update,context):
+    update.callback_query.answer()
+    filename = ((str(update.callback_query.data).split("#"))[1])
+    # Set the source and destination folder paths
+    src_folder = './detected_list'
+    dst_folder = './guest_list'
+
+    
+    # Set the source and destination file paths
+    src_path = os.path.join(src_folder, filename)
+    dst_path = os.path.join(dst_folder, filename)
+
+    # Move the photo from the source folder to the destination folder
+    shutil.move(src_path, dst_path)
+   
+
+  
 
 
 def main():
     """Start the bot."""
     updater = Updater(TOKEN)
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    # updater.dispatcher.add_handler(MessageHandler(Filters.text,reply))
+    updater.dispatcher.add_handler(CallbackQueryHandler(trigger_alarm, pattern='1'))
+
+
 
     # updater.start_webhook(listen="0.0.0.0",
     #                       port=int(PORT),
